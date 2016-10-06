9a36aad80258428be8b977713f52223390559fea
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2016-10-06 20:30:54 +0200

    we now store if panel should be open or not

--- admin/components/sw-onpage-options.js
+++ admin/components/sw-onpage-options.js
@@ -15,7 +15,26 @@
       staticWeb.retrieveTemplate("sw-onpage-options", function (template) {
         var elements = staticWeb.elements["sw-onpage-options"].instances;
         for (var i = 0; i < elements.length; i++) {
-          staticWeb.insertTemplate(template, elements[i]);
+          var element = elements[i];
+          var tmp = template.cloneNode(true);
+
+          var appDisplay = staticWeb.config.onPage.display;
+          var userDisplay = staticWeb.getUserSetting('sw.config.onPage.display');
+
+          staticWeb.insertTemplate(tmp, element);
+
+          var optionsCheckbox = element.querySelector('#sw-panel-left-checkbox');
+          var optionsLabel = element.querySelector('.sw-panel-dragdown label');
+          if (appDisplay == 'always' || userDisplay == 'always') {
+            optionsCheckbox.checked = true;
+          }
+
+          optionsCheckbox.addEventListener('change', function (e) {
+            var value = optionsCheckbox.checked ? 'always' : 'onDemand';
+            staticWeb.setSetting('sw.config.onPage.display', value);
+            return;
+          });
+          
           staticWeb.loadComponents();
         }
       });
