b75eee4b8ffb7d11c66e2959f594137553609aae
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2016-10-10 20:36:12 +0200

    changed so that we only use iframe if image fails

--- admin/components/sw-onpage-options-pages.js
+++ admin/components/sw-onpage-options-pages.js
@@ -133,19 +133,27 @@
 
                         // If we have an preview image for page we want to show it
                         var img = templateNode.querySelector('img');
+                        var iframe = templateNode.querySelector('iframe');
+
                         if (img) {
-                            img.onerror = function() {
+                            img.onerror = function () {
                                 // If no preview image can be found, hide img element.
                                 this.style.display = 'none';
+                                // and show template in iframe
+                                var iframe = this.nextElementSibling;
+                                if (iframe) {
+                                    // as no preview image was set for templat, show template in iframe
+                                    iframe.setAttribute('src', iframe.getAttribute('data-staticweb-src'));
+                                    iframe.style.display = 'block';
+                                }
                             }
                             img.setAttribute('src', previewImagePath);
                         }
 
-                        // By default we will display page in iframe to get a preview (can be overwritten by img with same name)
-                        // We will use sandbox tag to ensure no scripts are beign executed
+                        // we want o store
                         var iframe = templateNode.querySelector('iframe');
                         if (iframe) {
-                            iframe.setAttribute('src', path);
+                            iframe.setAttribute('data-staticweb-src', path);
                         }
 
                         templateContainer.appendChild(templateNode);
